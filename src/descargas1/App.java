561c779fa280e56e8781115c024749674f088a41
NicoCI <ibisshagrat@hotmail.com>
2020-11-20 08:21:29 +0100

    Se añade utils

--- src/descargas1/App.java
+++ src/descargas1/App.java
@@ -27,67 +27,6 @@ public class App {
 		listaDescargas.add(new Musica(3, 150, "Bailandose", new Cantante("Pum Pum Chim")));
 		//Conexion
 		ConexionRed conexion = new ConexionRed();
-		System.out.println(construirInforme(listaDescargas, conexion));
+		System.out.println(Utils.construirInforme(listaDescargas, conexion));
 	}
-	
-	public static String construirInforme(Collection<IdentificableDescargable> listaDescargas, Conexion conexion) {
-		String mensaje = "";
-		double tamanoTotal = calcularTamanoTotal(listaDescargas);
-		ordenarIdentificable((List<IdentificableDescargable>)listaDescargas);
-		for (IdentificableDescargable c : listaDescargas) {
-			mensaje += c.getId() + "\n";
-		}
-		mensaje += "Tamano total: " + tamanoTotal + "\n";
-		mensaje += "El tiempo de descarga estimado es: " + formatearTiempo(tamanoTotal);
-		return mensaje;	
-	}
-	
-	public static double calcularTamanoTotal(Collection<IdentificableDescargable> descargables){
-		double resultado = 0;
-		Descargable descargable;
-		for (IdentificableDescargable s : descargables) {
-			descargable = (Descargable) s;
-			resultado += descargable.getTamano();
-		}
-		return resultado;
-	}
-	
-	public static double calcularTiempoDescarga(Collection<IdentificableDescargable> descargables, Conexion conexion) {
-		
-		return calcularTamanoTotal(descargables)/conexion.getVelocidad();
-		
-	}
-	
-	public static void ordenarIdentificable(List<IdentificableDescargable> identificables) {
-		
-		class OrdenarPorId implements Comparator<IdentificableDescargable> {
-			
-			@Override
-			public int compare(IdentificableDescargable obj1, IdentificableDescargable obj2) {
-				int id1 = obj1.getId();
-				int id2 = obj2.getId();
-				
-				return id1 - id2;
-			}
-		}
-		
-		Collections.sort(identificables, new OrdenarPorId());
-	}
-	
-	public static String formatearTiempo(double segundos) {
-		String mensajeSalida = "";
-		int minutos = 0;
-		int horas = 0;
-		if (segundos >= 60) {		//Se observa si hay minutos
-			minutos = (int) segundos/60;
-			segundos += -minutos*60;
-			if (minutos >= 60) {
-				horas = minutos/60;
-				minutos += -horas*60;
-			}
-		}
-		mensajeSalida = horas + ":" + minutos + ":" + segundos;
-		return mensajeSalida;
-	}
-	
 }
--- src/descargas2y3/App.java
+++ src/descargas2y3/App.java
@@ -42,54 +42,6 @@ public class App {
 		listaDescargas.add(new Fichero(2, 15000, "01x23Peli2", new Saga("El señor del los Ladrillos")));
 		//Conexion
 		ConexionRed conexion = new ConexionRed();
-		System.out.println(construirInforme(listaDescargas, conexion));
+		System.out.println(Utils.construirInforme(listaDescargas, conexion));
 	}
-	
-	public static String construirInforme(Collection<IdentificableDescargable<?>> listaDescargas, Conexion conexion) {
-		String mensaje = "";
-		double tamanoTotal = calcularTamanoTotal(listaDescargas);
-		ordenarIdentificable(listaDescargas);
-		for (Identificable<?> c : listaDescargas) {
-			mensaje += c.getId() + "\n";
-		}
-		mensaje += "Tamano total: " + tamanoTotal + "\n";
-		mensaje += "El tiempo de descarga estimado es: " + formatearTiempo(tamanoTotal);
-		return mensaje;	
-	}
-	
-	public static double calcularTamanoTotal(Collection<IdentificableDescargable<?>> descargables){
-		double resultado = 0;
-		Descargable descargable;
-		for (Descargable s : descargables) {
-			descargable = (Descargable) s;
-			resultado += descargable.getTamano();
-		}
-		return resultado;
-	}
-	
-	public static double calcularTiempoDescarga(Collection<IdentificableDescargable<?>> descargables, Conexion conexion) {
-		return calcularTamanoTotal(descargables)/conexion.getVelocidad();
-		
-	}
-	
-	public static void ordenarIdentificable(Collection<IdentificableDescargable<?>> identificables) {
-		Collections.sort((List<IdentificableDescargable<?>>) identificables, new Identificable.Compare());
-	}
-	
-	public static String formatearTiempo(double segundos) {
-		String mensajeSalida = "";
-		int minutos = 0;
-		int horas = 0;
-		if (segundos >= 60) {			//Se observa si hay minutos
-			minutos = (int) segundos/60;
-			segundos += -minutos*60;
-			if (minutos >= 60) {		//Se observa si hay horas
-				horas = minutos/60;
-				minutos += -horas*60;
-			}
-		}
-		mensajeSalida = horas + ":" + minutos + ":" + segundos;
-		return mensajeSalida;
-	}
-	
 }
